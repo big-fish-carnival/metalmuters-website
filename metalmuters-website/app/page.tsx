659f00a01468805fd5c78179c5cf2436955b77f9
ghosalya <12974269+ghosalya@users.noreply.github.com>
2026-04-06 13:07:49 +0800

    Add email icon

--- metalmuters-website/app/page.tsx
+++ metalmuters-website/app/page.tsx
@@ -164,11 +164,27 @@ export default function Home() {
                   </svg>
                 </span>
               </a>
+              <a
+                href="mailto:bigfishcarnival@gmail.com"
+                target="_blank"
+                data-twe-ripple-init
+                data-twe-ripple-color="light"
+                className="mx-4 mb-2 inline-block rounded-full bg-white p-3 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg">
+                <span className="[&>svg]:h-8 [&>svg]:w-8">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="#26203e"
+                    viewBox="0 0 512 512">
+                    <path
+                      d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" />
+                  </svg>
+                </span>
+              </a>
             </div>
           </div>
         </div>
         <div className="flex flex-1 w-full flex-col items-center justify-between bg-[#26203e]">
-          <div className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between mb-4">
+          <div className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between m-4 p-2">
                 <p>© 2026 Big Fish Carnival. All Rights Reserved</p>
           </div>
         </div>
